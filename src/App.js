e3b9e4a3190c023cb8e5018d1e0445a029963217
harpreet1602 <harpreets1602@gmail.com>
2021-08-01 20:50:07 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
-
+import {useEffect} from "react";
+import { firestore } from './firebase';
 function App() {
+  useEffect( ()=>{
+    let f = async () =>{
+    
+    let querySnapshot = await firestore.collection("posts").limit(3).get();
+    querySnapshot.forEach((doc)=> console.log(doc.id));      
+  }
+    f() 
+    
+      }  ,[])
+
+
+
+
   return (
     <div>
-      
+
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,20 @@
-import fire
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDJ4VRzDPHl18gUB0jKVdSMWC4M60QfQzk",
+    authDomain: "reels-e08fe.firebaseapp.com",
+    projectId: "reels-e08fe",
+    storageBucket: "reels-e08fe.appspot.com",
+    messagingSenderId: "1082545881711",
+    appId: "1:1082545881711:web:ec9e62697a11a8f9f5c39a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+export const firestore = firebase.firestore();
+
+export default firebase;
